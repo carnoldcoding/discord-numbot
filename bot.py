1367e1fa10c4fedfc823938a673ad7706e4e930f
Cameron Arnold <carnold.information@gmail.com>
2026-02-26 12:41:29 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,5 +7,7 @@ WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
 number = random.randint(0, 9)
 # number2 = random.randint(0,9)
 
-requests.post(WEBHOOK_URL, json={"content": str(number) + ", " + str(number2)})
+requests.post(WEBHOOK_URL, json={"content": str(number)})
+# requests.post(WEBHOOK_URL, json={"content": str(number) + ", " + str(number2)})
+
 
